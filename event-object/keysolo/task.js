320f5d11abc15dd35630aec1555f692a848789e5
AlexaKhiteva <alexakhiteva@inbox.ru>
2023-05-10 13:36:26 +0300

    Задача выполнена

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -26,6 +26,7 @@ class Game {
         this.fail();
       }
       });
+
       
       /*
       TODO:
@@ -102,41 +103,4 @@ class Game {
 
 new Game(document.getElementById('game'))
 
-const btnMenu = document.querySelector(".dropdown");
-const itemValue = document.querySelector(".dropdown__value");
-const activeMenu = document.querySelector(".dropdown__list");
 
-const lists = Array.from(document.querySelectorAll("dropdown__list"));
-const links = Array.from(document.querySelectorAll(".dropdown__link"));
-
-btnMenu.addEventListener("click", function () {
-    activeMenu.classList.toggle("dropdown__list_active");
-});
-
-for (let index = 0; index < links.length; index++) {
-    const link = links[index];
-    link.onclick = () => {
-        itemValue.textContent = link.textContent;
-        return false;
-    }
-}
-
-
-const tabs = Array.from(document.querySelectorAll(".tab"));
-const contents = Array.from(document.querySelectorAll(".tab__content"));
-
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", (event) => {
-        let tabsChildren = event.target.parentElement.children;
-        for (let t = 0; t < tabsChildren.length; t++) {
-            tabsChildren[t].classList.remove("tab_active");
-        }
-        tabs[i].classList.add("tab_active");
-        let tabContentChildren = event.target.parentElement.nextElementSibling.children;
-        for (let c = 0; c < tabContentChildren.length; c++) {
-            tabContentChildren[c].classList.remove("tab__content_active");
-        }
-        contents[i].classList.add("tab__content_active");
-
-    });
-}
