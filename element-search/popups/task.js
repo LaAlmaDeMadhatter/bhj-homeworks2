81505ff92ab20defe1552783fb78412465774cb4
AlexaKhiteva <alexakhiteva@inbox.ru>
2023-05-19 14:27:38 +0300

    Done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,18 @@
-const modal_main = document.getElementById('modal_main');
-modal_main.classList.add('modal_active');
+const modalMain = document.querySelector("#modal_main");
+const modalSuccess = document.querySelector("#modal_success");
+const good = document.querySelector(".show-success");
+const closed = Array.from(document.querySelectorAll("div.modal__close"));
 
-const modal__close = document.getElementsByClassName('modal__close');
-const modal_success = document.getElementById('modal_success');
-for (let i = 0; i < modal__close.length; i++) {
-    modal__close.item(i).onclick = () => {
-        modal_main.classList.remove('modal_active');
-        modal_success.classList.remove('modal_active');
-    };
-}
+modalMain.className = "modal modal_active";
 
-
-const show_success = document.getElementsByClassName('show-success').item(0);
-show_success.onclick = () => {
-    modal_main.classList.remove('modal_active');
-    modal_success.classList.add('modal_active');
+good.onclick = () => {
+  modalSuccess.className = "modal modal_active";
+  modalMain.className = "modal";
 };
+
+closed.forEach(function (element) {
+  const parent = element.closest(".modal");
+  element.onclick = function () {
+    parent.className = "modal";
+  };
+});
