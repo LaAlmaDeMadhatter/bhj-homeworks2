b2874e05ff8782d26b5e483a5d17f2d620f75a80
AlexaKhiteva <alexakhiteva@inbox.ru>
2023-05-23 13:50:37 +0300

    Done

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,84 +1,24 @@
-const font_size = document.getElementsByClassName('font-size');
-const book = document.getElementsByClassName('book').item(0);
+const fontControls = Array.from(document.getElementsByClassName("font-size"));
+const book = document.getElementById("book");
 
-const color = document.querySelectorAll('div.book__control_color a');
-const background = document.querySelectorAll('div.book__control_background a');
+fontControls.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    let target = event.target;
 
-for (let i = 0; i < font_size.length; i++) {
-    font_size.item(i).onclick = function(){return false};
-    color.item(i).onclick = function(){return false};
-    background.item(i).onclick = function(){return false};
-
-    font_size.item(i).addEventListener('click', function () {
-        if (!(font_size.item(i).classList.contains('font-size_active'))) {
-            for (let j = 0; j < font_size.length; j++) {
-                if (font_size.item(j).classList.contains('font-size_active')) {
-                    font_size.item(j).classList.remove('font-size_active');
-                }
-            }
-            font_size.item(i).classList.add('font-size_active');
-        }
-
-        if (book.classList.contains('book_fs-big')) {
-            book.classList.remove('book_fs-big');
-        }
-
-        if (book.classList.contains('book_fs-small')) {
-            book.classList.remove('book_fs-small');
-        }
-
-        if (font_size.item(i).dataset.size) {
-            book.classList.add('book_fs-' + font_size.item(i).dataset.size);
-        }  
+    document.querySelectorAll(".font-size").forEach((element) => {
+      element.classList.remove("font-size_active");
     });
 
-    color.item(i).addEventListener('click', function () {
-        if (!(color.item(i).classList.contains('color_active'))) {
-            for (let j = 0; j < color.length; j++) {
-                if (color.item(j).classList.contains('color_active')) {
-                    color.item(j).classList.remove('color_active');
-                }
-            }
-            color.item(i).classList.add('color_active');
-        }
+    target.classList.add("font-size_active");
 
-       if (book.classList.contains('book_color-gray')) {
-        book.classList.remove('book_color-gray');
-       }
-
-       if (book.classList.contains('book_color-whitesmoke')) {
-        book.classList.remove('book_color-whitesmoke');
-       }
-
-       if (book.classList.contains('book_color-black')) {
-        book.classList.remove('book_color-black');
-       }
-
-        book.classList.add('book_color-' + color.item(i).dataset.textColor);  
-    });
-
-    background.item(i).addEventListener('click', function () {
-        if (!(background.item(i).classList.contains('color_active'))) {
-            for (let j = 0; j < background.length; j++) {
-                if (background.item(j).classList.contains('color_active')) {
-                    background.item(j).classList.remove('color_active');
-                }
-            }
-            background.item(i).classList.add('color_active');
-        }
-
-        if (book.classList.contains('book_bg-gray')) {
-            book.classList.remove('book_bg-gray');
-        }
-
-        if (book.classList.contains('book_bg-black')) {
-        book.classList.remove('book_bg-black');
-        }
-
-        if (book.classList.contains('book_bg-white')) {
-        book.classList.remove('book_bg-white');
-        }
-
-        book.classList.add('book_bg-' + background.item(i).dataset.bgColor);
-    });
-}
+    if (target.classList.contains("font-size_small")) {
+      book.classList.add("book_fs-small");
+    } else if (target.classList.contains("font-size_big")) {
+      book.classList.add("book_fs-big");
+      book.classList.remove("book_fs-small");
+    } else {
+      book.classList.remove("book_fs-small", "book_fs-big");
+    }
+  });
+});
